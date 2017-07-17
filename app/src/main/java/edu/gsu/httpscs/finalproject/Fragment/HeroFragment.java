f8e432c1c49bf3d09bc7bd493434c593fb57a179
Tong Tian <ttian4@student.gsu.edu>
2017-07-16 23:00:29 -0400

    23:00

--- app/src/main/java/edu/gsu/httpscs/finalproject/Fragment/HeroFragment.java
+++ app/src/main/java/edu/gsu/httpscs/finalproject/Fragment/HeroFragment.java
@@ -13,10 +13,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.Toast;
-
-import butterknife.ButterKnife;
-import edu.gsu.httpscs.finalproject.GameActivity;
-import edu.gsu.httpscs.finalproject.GameActivity_ViewBinding;
 import edu.gsu.httpscs.finalproject.R;
 
 /**
